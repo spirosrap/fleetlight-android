b05be24f5bd0d1bfa6801c66c80112cd4c1f4c75
Spiros Raptis <heptapod@posteo.eu>
2026-07-22 17:03:12 +0300

    Release Android 1.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "app.fleetlight.mobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.4.2"
+        versionCode = 8
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
